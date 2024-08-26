9ce41547aa6bd113b44ae03b61c786b645b0ad04
Yousif <yoarafa104@gmail.com>
2024-08-26 16:43:58 +0200

    primo commit progetto

---
+++ src/main/java/com/example/demo/controller/homeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class homeController {
+
+    @GetMapping ("/")
+    public String index(){
+        return "index";
+    }
+
+}
