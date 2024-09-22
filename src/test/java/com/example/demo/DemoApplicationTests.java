af4a37a786fba095b4ff1be9fac9361b3476a823
Yousif <yoarafa104@gmail.com>
2024-09-22 13:47:01 +0200

    Inizzializzazione del progetto

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DemoApplicationTests {
 
 	@Test
